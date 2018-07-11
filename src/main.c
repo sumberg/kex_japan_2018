9a790e3ef2ecf46ee53f792f0934bfbda47cf0b1
sieken <davhen@kth.se>
2018-07-11 17:00:41 +0900

    Update ROM to write back results to RAM

--- src/main.c
+++ src/main.c
@@ -7,41 +7,69 @@
 #include "SPI.h"
 #include "rom.h"
 
-int main() {
-	_delay_ms(500); // TODO remove this?
+/* Sets necessary ports and starts slave program */
+void start_slave(void);
+/* Stops slave execution, and gives control of all components to master */
+void stop_slave(void);
+
+int main()
+{
 	setup();
 
-	uint8_t nxtByte;
-	uint16_t addr = 0x8000;
-	uint8_t addr_hi, addr_lo;
+	SPI_write(IODIRA, 0xFF);
+	ROM_TO_RAM();
 
-	/* Write entire ROM to RAM chip, from starting address 0x8000 (mask 0x7FFF)*/
-	while (ROM_instructionsLeft()) {
-		nxtByte = ROM_fetchNextByte();
-		addr_hi = ((addr & 0xFF00) >> 8);
-		addr_lo = (addr & 0x00FF);
-		RAM_write(addr_hi, addr_lo, nxtByte);
-		addr++;
+	start_slave();
+
+	uint16_t stop;
+	uint8_t times;
+	while(1) {
+		stop = SPI_read(GPIOA) << 8;
+		stop |= SPI_read(GPIOB);
+		stop |= 0x8000;
+		if (stop >= 0xFF00 && stop <= 0xFF02) break;
 	}
 
-	/* Verify RAM write */
-	addr = 0x8000;
-	ROM_resetPC();
-	uint8_t tmp;
-	uint8_t success = 1;
-	while (ROM_instructionsLeft()) {
-		nxtByte = ROM_fetchNextByte();
-		addr_hi = ((addr & 0xFF00) >> 8);
-		addr_lo = (addr & 0x00FF);
-		tmp = RAM_read(addr_hi, addr_lo);
-		addr+++;
-		if (tmp != nxtByte)
-			success = 0;
+	stop_slave();
+
+	// uint8_t success = 1;
+	// if (RAM_read(0x85, 0x00) != 0xAA) success = 0;
+	// if (RAM_read(0x85, 0x01) != 0x55) success = 0;
+	// if (RAM_read(0x85, 0x02) != 0x66) success = 0;
+	// if (RAM_read(0x85, 0x03) != 0xFF) success = 0;
+
+	// DEBUG_PORT |= (success << DEBUG_LED);
+	uint8_t read = RAM_read(0x85, 0x00);
+	read /= 10;
+	while(1) {
+		for (int i = read; i > 0; i--) {
+			DEBUG_PORT |= (1 << DEBUG_LED);
+			_delay_ms(500);
+			DEBUG_PORT &= ~(1 << DEBUG_LED);
+			_delay_ms(500);
+		}
+		_delay_ms(1000);
 	}
-
-	PORTB |= (success << DEBUG_LED);
-
-	while(1) {}
 
 	return 0;
+}
+
+void start_slave(void)
+{
+	DATA_DDR = 0x00;
+	SPI_write(IODIRA, 0xFF);
+	SPI_write(IODIRB, 0xFF);
+	RAM_CTRL_PORT &= ~((1 << RAM_RW) | (1 << RAM_CE));
+
+	CTRL_PORT |= (1 << CTRL_RESET_SLAVE);
+}
+
+void stop_slave(void)
+{
+	DATA_DDR = 0xFF;
+	SPI_write(IODIRA, 0x00);
+	SPI_write(IODIRB, 0x00);
+	RAM_CTRL_PORT |= (1 << RAM_RW) | (1 << RAM_CE);
+
+	CTRL_PORT &= ~(1 << CTRL_RESET_SLAVE);
 }
